a8148f9dd9e238d7affe5ed3b2b8e8dd310301cc
Robert Laws <roblaws@me.com>
2018-04-30 20:12:41 +0300

    passed tests

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
 const app = "I don't do much.";
+const token = '8bf7325cfe1b7abe80033a338c366448ea295ca3';
+
+fetch('https://api.github.com/user/repos', {
+  headers: {
+    Authorization: `token ${token}`
+  }
+}).then(res => res.json()).then(json => console.log(json));
